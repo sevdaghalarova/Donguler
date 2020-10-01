4e36ea1e42de355beeadd375d6cb2bac67137a9e
sevdaghalarova <51816139+sevdaghalarova@users.noreply.github.com>
2020-10-02 00:49:47 +0400

    range() fonksiyonu

---
+++ range_fonk.py
@@ -0,0 +1,7 @@
+print(*range(1,20)) # 1-den 20'e kadar sayilari bastiracak
+# not: * yazmazsak bastirmayacaktir
+print(*range(1,20,2)) # 1-den 20-e kadar 2-2 atlayarak git
+print(*range(20,0,-1)) # 20-den 0-a kadar git
+
+for i in range(1,100): # 1-100 arasinda donecek
+    print(i)
